2875fc898474c4f8f9d1980190f925e948d34645
selam lemma <oneiconforlife26@gmail.com>
2020-08-28 18:53:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(arrays)
+arrays.sort
+end
 
+def sort_array_desc(array_b)
+array_b.sort{}
+end
+
+def sort_array_char_count(char_count)
+  char_count.sort{|x,y| x <=> y}
+end
+
+
+def swap_elements(array_swap)
+ swap_elements =  ["blake", "ashley", "scott"]
+   swap_elements[1],swap_elements[2] = swap_elements[2],swap_elements[1]
+end
+
+
+def reverse_array(array_rev)
+   array_rev.reverse
+end
+
+
+def kesha_maker(array_maker)
+
+end
+
+
+
+def find_a(array_a)
+  array_a.detect {|i| array.first(i)}
+
+  end
+
+
+  def sum_array(array_su)
+  array_su.inject(0) {|sum, i| sum + i}
+  end
+
+
+
+  def add_s(array_add)
+  array_add.inject (s)
+  end
